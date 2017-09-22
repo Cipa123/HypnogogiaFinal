da1575e7c0975d576be773b7b6b1b197c8497123
Cipa123 <mykolas.tamonis@gmail.com>
2017-09-22 16:18:42 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+public class Main
+{
+	private static GameFrame game;
+	private static GameKeyListener listener; 
+	public static void main(String[] args)
+	{
+		game = new GameFrame();
+		GameKeyListener listener = new GameKeyListener(game);
+		game.addKeyListener(listener);
+		
+	}
+}
 
