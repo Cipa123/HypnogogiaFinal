e5f30d83e1aba7eb7176a8b7cd99bb43cb48856d
Cipa123 <mykolas.tamonis@gmail.com>
2017-09-22 18:00:19 -0600

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,13 +1,26 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameFrame extends JFrame 
 {
-	public GameFrame()
+	private GameGraphics g;
+	private Player player;
+	
+	public GameFrame(Player p)
 	{
+		
 		super("Hypnogogia");
+		player = p;
 		this.setSize(1920, 1080);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//this.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		this.setUndecorated(true);
 		this.setVisible(true);
+		JPanel masterPanel = new JPanel();
+		this.add(masterPanel);
+		
+		GameGraphics g = new GameGraphics(this,p);
+		
+		
+		
 	}
 }
